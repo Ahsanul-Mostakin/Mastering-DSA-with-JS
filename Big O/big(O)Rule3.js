1ad5eb68ead9204bdaf38fc7934a45794cf28dcc
Ahsanul Mostakin <ahsanulmostakin1111@gmail.com>
2024-10-25 00:21:16 +0600

    Rule 3 compressBoxesTwice

---
+++ Big O/big(O)Rule3.js
@@ -0,0 +1,11 @@
+function compressBoxesTwice(boxes, boxes2) {
+    boxes.forEach(function(box) {
+      console.log(box);
+    });
+  
+    boxes2.forEach(function(box) {
+      console.log(box);   
+  
+    });
+  }
+  // O (a + b) cause two boxes
